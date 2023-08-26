36266178cf09e49ae7e205522e6edf355605fec5
giirrr <lim966010@gamil.com>
2023-08-26 18:12:43 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  // console.log("i run all the time");
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  /*   const iRunOnlyOnce = () => {
+    console.log("i run only once");
+  };
+  useEffect(iRunOnlyOnce, []); */
+  useEffect(() => {
+    console.log("CALL THE API. . . .");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
+
+  return (
+    <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here. . ."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
+}
+
+export default App;
