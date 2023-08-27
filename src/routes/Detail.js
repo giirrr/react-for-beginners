033e78d991eb8db2967fa9295c49b8d561595048
giirrr <ikl1121@naver.com>
2023-08-27 22:12:15 +0900

    Add movie detailPage

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import MovieDetail from "../components/MovieDetail";
 function Detail() {
+  const [loading, setLoading] = useState("true");
+  const [movie, setMovie] = useState({});
   const { id } = useParams();
   const getMovie = async () => {
-    // id를 알고 있으니까 API로부터 정보를 fetch 해올 수 있다
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    console.log(json.data.movie);
+    setMovie(json.data.movie);
+    setLoading(false);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  console.log(id);
-  return <h1>Detail</h1>;
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <MovieDetail
+            title={movie.title_long}
+            src={movie.medium_cover_image}
+            description={movie.description_full}
+            genres={movie.genres}
+            url={movie.url}
+            rating={movie.rating}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
 export default Detail;
