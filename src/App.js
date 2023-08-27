f31d13da9fdaad50a6713a1a740bbe16973e708b
giirrr <lim966010@gamil.com>
2023-08-27 16:55:33 +0900

    Change USD->Coin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [input, setInput] = useState("");
+  const [selectedCoin, setSelectedCoin] = useState("");
+  const [inverted, setInverted] = useState(false);
+  const selectCoin = (event) => setSelectedCoin(event.target.value);
+  const converter = (event) => setInput(event.target.value);
+  const onInvert = () => {
+    reset();
+    setInverted((current) => !current);
+  };
+  const reset = () => setInput("");
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -13,18 +23,38 @@ function App() {
   }, []);
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      <h1>Coin List {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
+        <select onChange={selectCoin}>
           {coins.map((coin) => (
-            <option>
-              {coin.name}({coin.symbol}): ${coin.quotes.USD.price} USD
+            <option value={coin.quotes.USD.price}>
+              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
             </option>
           ))}
         </select>
       )}
+      <hr />
+      <h3>USD & Selected Coin Inverter</h3>
+      <label htmlFor="myUsd">My USD</label>
+      <input
+        id="myUsd"
+        type="number"
+        value={inverted ? selectedCoin * input : input}
+        onChange={converter}
+        disabled={inverted}
+      />
+      <label htmlFor="btc"> Selected Coins</label>
+      <input
+        id="btc"
+        type="number"
+        value={inverted ? input : input / selectedCoin}
+        onChange={converter}
+        disabled={!inverted}
+      />
+      <button onClick={onInvert}>Invert</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
