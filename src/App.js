14e7d1d9fafdd09306a37c17948d03cff8675bcb
giirrr <lim966010@gamil.com>
2023-08-27 19:56:52 +0900

    Movie app 2

--- src/App.js
+++ src/App.js
@@ -1,44 +1,5 @@
-import { useState, useEffect } from "react";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log(movies);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            // map 을 사용할 때 마다 key를 Element에 줘야함
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  // 고유한 값이기만 하다면 key값에 g를 넣어도 상관없음
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
+
 export default App;
